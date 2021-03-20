4d8187bc18a3cda95ac0cd8a3ac26d666185b42f
jayars <jayars@local>
2021-03-20 13:08:23 +0800

    repro

---
+++ readme.md
@@ -0,0 +1,7 @@
+# @tailwind/jit gradientColorStop bug
+
+Build using `npm run build:css`
+
+Turn `@tailwind/jit` on/off in `postconfig.config.js`
+
+see [tailwind-jit-gradient-bug.mp4](tailwind-jit-gradient-bug.mp4) for repro
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './index.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      gradientColorStops: {
+        x: '#b1eafc',
+        y: '#fbd7f8'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
